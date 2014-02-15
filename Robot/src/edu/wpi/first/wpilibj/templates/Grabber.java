50ba4b98c3dae84c9dc6127b2ba6fb49332ed8e1
JacobLaurendeau <jakellaurendeau@gmail.com>
2014-02-15 13:08:46 -0500

    Fuctional driving test

--- Robot/src/edu/wpi/first/wpilibj/templates/Grabber.java
+++ Robot/src/edu/wpi/first/wpilibj/templates/Grabber.java
@@ -5,8 +5,8 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -14,11 +14,18 @@ import edu.wpi.first.wpilibj.RobotDrive;
  */
 public class Grabber {
 
-    private RobotDrive drive = new RobotDrive(1, 2);
+    private int pinGrabberLeft = 1;
+    private int pinGrabberRight = 2;
+
+    private RobotDrive drive = new RobotDrive(pinGrabberLeft, pinGrabberRight);
+
+    private int direction;
+    private float speed = 0.95f;
+
     
-    int direction;
-    float speed = 0.95f;
-    
+    public Grabber() {
+        SmartDashboard.putNumber("grabberSpeed", speed);
+    }
     
     public void takeInput(int direction) {
         this.direction = direction;
@@ -26,20 +33,25 @@ public class Grabber {
 
     public void update() {
         drive.setSafetyEnabled(false);
+
+        if(!RobotTemplate.driveSticks.getLeftJoystick().getRawButton(7) && !RobotTemplate.driveSticks.getRightJoystick().getRawButton(7)) {
+
+            //Set wheel speed
+            if (direction > 0) {
+                drive.tankDrive(speed, speed);
+            } else if (direction < 0) {
+                drive.tankDrive(-speed, -speed);
+            } else {
+                //stopped
+                drive.tankDrive(0, 0);
+            }
         
-        
-        //Set wheel speed
-        if (direction > 0) {
-            drive.tankDrive(speed, -speed);
-        } else if (direction < 0) {
-            drive.tankDrive(-speed, speed);
-        } else {
-            //stopped
+        }
+        else {
             drive.tankDrive(0, 0);
         }
-        
+
         direction = 0;
-                
 
     }
 }
