8cd30562e83a84dd53ade030bc33f933cb36d580
JacobLaurendeau <jakellaurendeau@gmail.com>
2014-02-26 19:43:33 -0500

    MADE CONTROLS NEW

--- Robot/src/edu/wpi/first/wpilibj/templates/Autonomous.java
+++ Robot/src/edu/wpi/first/wpilibj/templates/Autonomous.java
@@ -11,28 +11,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Autonomous {
 
-    private static final double distance = 9.4;
-    private static float autoLeft = -0.5f;
-    private static float autoRight = -0.57f;
+    private static final double distance = 9.4; //apparently this is the number of meters we must travel
+    private static float autoLeft = -0.5f;  //speed of the left wheels, also they're negative, cus the robot is wired that way
+    private static float autoRight = -0.57f;    //speed of the right wheels, it's slightly different cuz the robot drives weird
     private static double leftDistance = 0.0;
     private static double rightDistance = 0.0;
     public Autonomous()
     {
         SmartDashboard.putNumber("autoLeft", autoLeft);     //attempted placing values onto smartdashboard
         SmartDashboard.putNumber("autoRight", autoRight);   //note: we aren't very good at this
+        RobotTemplate.wheels.leftEncoder.setDistancePerPulse(1 / 636.61);
+        RobotTemplate.wheels.rightEncoder.setDistancePerPulse(1 / 636.61);
     }
     public void update() 
     {
-        //the conversion factor between meters and tics. is 1 meter = 636.61 tics(which is what encoders measure)
-        RobotTemplate.wheels.leftEncoder.setDistancePerPulse(1 / 636.61);       //once the code is pushed to git
-        RobotTemplate.wheels.rightEncoder.setDistancePerPulse(1 / 636.61);      //place these lines within the constructor
-        
+        //the conversion factor between meters and tics. is 1 meter = 636.61 tics(which is what encoders measure)   
+
         leftDistance = Math.abs(RobotTemplate.wheels.leftEncoder.getDistance());    //use absolute value
         rightDistance = Math.abs(RobotTemplate.wheels.rightEncoder.getDistance());  //for comparison purposes
         
         //test this on raw values then on distance values if possible.
         if (leftDistance < distance || rightDistance < distance) {
-            RobotTemplate.wheels.simpleDrive(autoLeft, autoRight);    //negative values because the robot was wired that way
+            RobotTemplate.wheels.simpleDrive(autoLeft, autoRight); 
         } else {
             RobotTemplate.wheels.simpleDrive(0, 0);             //this part signifies that the robot stop moving
         }//end else                                             //this should occur once the robot has moved 9.4 meters.
